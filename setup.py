3bfaf60ddff087c45e4212f681e31c2db62b7ee5
Sebastiaan Zeeff <sebastiaan.zeeff@gmail.com>
2020-09-12 11:58:48 +0200

    Bump version to 0.0.1-dev1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="async-rediscache",
-    version="0.0.1-dev0",
+    version="0.0.1-dev1",
     description="An easy to use asynchronous Redis cache",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
