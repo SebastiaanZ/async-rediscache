de317d6de9bc718e3d575ff07f0a0af8b2c0b1dd
Sebastiaan Zeeff <sebastiaan.zeeff@gmail.com>
2020-12-17 21:10:48 +0100

    Add tests for RedisTaskQueue data type
    Signed-off-by: Sebastiaan Zeeff <sebastiaan.zeeff@gmail.com>

--- tests/types/test_queue.py
+++ tests/types/test_queue.py
@@ -9,7 +9,7 @@ class RedisQueueTests(BaseRedisObjectTests):
 
     async def asyncSetUp(self):
         await super().asyncSetUp()
-        self.queue = types.RedisQueue(namespace="test_cache")
+        self.queue = types.RedisQueue(namespace="test_queue")
 
     async def test_put_get_no_wait(self):
         """Test the .put and .get method without waiting, open connections."""
@@ -96,3 +96,197 @@ class RedisQueueTests(BaseRedisObjectTests):
         await self.queue.get()
         self.assertEqual(await self.queue.qsize(), 0)
         self.assertTrue(await self.queue.empty())
+
+
+class RedisTaskQueueTests(BaseRedisObjectTests):
+    """Tests for teh RedisTaskQueue class."""
+
+    async def asyncSetUp(self):
+        await super().asyncSetUp()
+        self.queue = types.RedisTaskQueue(namespace="task_queue")
+
+    async def test_get_also_pushes_task_to_pending(self):
+        """Test if the `.get` also pushes the task to the pending tasks queue."""
+        test_value = "Python Discord"
+        await self.queue.put(test_value)
+        task = await self.queue.get()
+
+        with await self.mock_session.pool as connection:
+            queue_length = await connection.llen(self.queue.namespace)
+            pending_length = await connection.llen(self.queue.namespace_pending)
+
+        self.assertEqual(task.value, test_value)
+        self.assertEqual(queue_length, 0)
+        self.assertEqual(pending_length, 1)
+
+    async def test_task_finalize_correctly_finalizes_task(self):
+        """`task.finalize` should remove the task from the pending queue and marks it as done.`"""
+        test_value = "Python Discord"
+        await self.queue.put(test_value)
+        self.assertEqual(await self.queue.qsize(), 1)
+
+        task = await self.queue.get()
+        self.assertEqual(await self.queue.qsize(), 0)
+        self.assertEqual(task.value, test_value)
+
+        with await self.mock_session.pool as connection:
+            pending_length = await connection.llen(self.queue.namespace_pending)
+
+        self.assertEqual(pending_length, 1)
+
+        await task.finalize()
+
+        with await self.mock_session.pool as connection:
+            pending_length = await connection.llen(self.queue.namespace_pending)
+
+        self.assertEqual(pending_length, 0)
+        self.assertTrue(task.done)
+
+    async def test_reschedule_task_puts_task_back_on_queue(self) -> None:
+        """Rescheduling a task should put it back on the queue."""
+        test_value = "Python Discord"
+        await self.queue.put(test_value)
+        self.assertEqual(await self.queue.qsize(), 1)
+
+        task = await self.queue.get()
+        self.assertEqual(await self.queue.qsize(), 0)
+        self.assertEqual(task.value, test_value)
+
+        with await self.mock_session.pool as connection:
+            pending_length = await connection.llen(self.queue.namespace_pending)
+        self.assertEqual(pending_length, 1)
+
+        await self.queue.reschedule_pending_task(task)
+
+        self.assertEqual(await self.queue.qsize(), 1)
+        with await self.mock_session.pool as connection:
+            pending_length = await connection.llen(self.queue.namespace_pending)
+        self.assertEqual(pending_length, 0)
+
+    async def test_reschedule_all_tasks_should_requeue_all_tasks(self) -> None:
+        """Reschedule all tasks should reschedule all tasks in the original order."""
+        task_values = [f"task-{ident}" for ident in range(10)]
+
+        for task_value in task_values:
+            await self.queue.put(task_value)
+
+        self.assertEqual(await self.queue.qsize(), len(task_values))
+
+        task_values_iter = iter(task_values)
+        async for task in self.queue:
+            self.assertEqual(next(task_values_iter), task.value)
+
+        self.assertEqual(await self.queue.qsize(), 0)
+        with await self.mock_session.pool as connection:
+            pending_length = await connection.llen(self.queue.namespace_pending)
+        self.assertEqual(pending_length, 10)
+
+        await self.queue.reschedule_all_pending_client_tasks()
+
+        self.assertEqual(await self.queue.qsize(), 10)
+        with await self.mock_session.pool as connection:
+            pending_length = await connection.llen(self.queue.namespace_pending)
+        self.assertEqual(pending_length, 0)
+
+    async def test_task_raises_TaskAlreadyDone_for_second_finalize(self) -> None:
+        """Should raise TaskAlreadyDone for finalizing a task that is already done."""
+        await self.queue.put("Hello")
+        task = await self.queue.get()
+        await task.finalize()
+
+        with self.assertRaises(types.TaskAlreadyDone):
+            await task.finalize()
+
+    async def test_task_queue_raises_TaskNotPending_for_finalizing_nonpending_task(self) -> None:
+        """Should raise TaskNotPending for finalizing a task not found in pending queue."""
+        await self.queue.put("Hello")
+        task = await self.queue.get()
+        await task.reschedule()
+
+        with self.assertRaises(types.TaskNotPending):
+            await task.finalize()
+
+    async def test_reschedule_task_by_value(self) -> None:
+        """We should also be able to reschedule a task by its value."""
+        value = "Some interesting value"
+        await self.queue.put(value)
+        self.assertEqual((await self.queue.get()).value, value)
+        await self.queue.reschedule_pending_task(value)
+
+        self.assertEqual(await self.queue.qsize(), 1)
+        with await self.mock_session.pool as connection:
+            pending_length = await connection.llen(self.queue.namespace_pending)
+        self.assertEqual(pending_length, 0)
+
+    async def test_rescheduling_unknown_task_raises_TaskNotPending(self) -> None:
+        """Rescheduling an unknown task raises TaskNotPending."""
+        with self.assertRaises(types.TaskNotPending):
+            await self.queue.reschedule_pending_task("non-existent task")
+
+    async def test_task_raises_RuntimeError_for_missing_owner_queue(self):
+        """If the owner queue instance no longer exists, a task should raise a RuntimeError."""
+        await self.queue.put("Hello")
+        task = await self.queue.get()
+        del self.queue
+        with self.assertRaises(RuntimeError):
+            _owner = task.owner
+
+
+class RedisTaskQueueMultipleClientsTests(BaseRedisObjectTests):
+    """Tests for teh RedisTaskQueue class."""
+
+    async def asyncSetUp(self):
+        await super().asyncSetUp()
+        self.queue_one = types.RedisTaskQueue(namespace="task_queue", client_identifier="one")
+        self.queue_two = types.RedisTaskQueue(namespace="task_queue", client_identifier="two")
+
+    async def test_client_pending_queues_are_independent(self) -> None:
+        """The pending queues of different clients should be independent."""
+        values = [f"task-{i}" for i in range(6)]
+
+        for value_one, value_two in zip(*[iter(values)]*2):
+            await self.queue_one.put(value_one)
+            await self.queue_two.put(value_two)
+
+        # The central queue should be the same
+        self.assertEqual(await self.queue_one.qsize(), len(values))
+        self.assertEqual(await self.queue_one.qsize(), await self.queue_two.qsize())
+
+        for value_one, value_two in zip(*[iter(values)]*2):
+            self.assertEqual((await self.queue_one.get()).value, value_one)
+            self.assertEqual((await self.queue_two.get()).value, value_two)
+
+        # The central queue should now be depleted
+        self.assertEqual(await self.queue_one.qsize(), 0)
+        self.assertEqual(await self.queue_one.qsize(), await self.queue_two.qsize())
+
+        with await self.mock_session.pool as connection:
+            pending_one_length = await connection.llen(self.queue_one.namespace_pending)
+            pending_two_length = await connection.llen(self.queue_two.namespace_pending)
+
+        self.assertEqual(pending_one_length, len(values) // 2)
+        self.assertEqual(pending_two_length, len(values) // 2)
+
+        await self.queue_one.reschedule_all_pending_client_tasks()
+
+        with await self.mock_session.pool as connection:
+            pending_one_length = await connection.llen(self.queue_one.namespace_pending)
+            pending_two_length = await connection.llen(self.queue_two.namespace_pending)
+
+        self.assertEqual(pending_one_length, 0)
+        self.assertEqual(pending_two_length, len(values) // 2)
+
+        # Check if the values of queue_one have made it back on the main queue
+        self.assertEqual(await self.queue_one.qsize(), len(values) // 2)
+
+        await self.queue_two.reschedule_all_pending_client_tasks()
+
+        with await self.mock_session.pool as connection:
+            pending_one_length = await connection.llen(self.queue_one.namespace_pending)
+            pending_two_length = await connection.llen(self.queue_two.namespace_pending)
+
+        self.assertEqual(pending_one_length, 0)
+        self.assertEqual(pending_two_length, 0)
+
+        # Check if the values of queue_two have made it back on the main queue
+        self.assertEqual(await self.queue_one.qsize(), len(values))
