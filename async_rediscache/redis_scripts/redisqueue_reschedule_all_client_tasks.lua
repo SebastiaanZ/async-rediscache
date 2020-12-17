588eee83a44083d17c08713f67fd686677a51541
Sebastiaan Zeeff <sebastiaan.zeeff@gmail.com>
2020-12-17 19:57:19 +0100

    Add version compatibility for Lua unpack function
    Differences in the Lua version used means that we either have to use the
    old `unpack` function or the newer `table.unpack` function. In order to
    dynamically adapt to the correct function, I've added a check that picks
    `table.unpack` if `unpack` is nil; otherwise it will use `unpack`.
    
    Signed-off-by: Sebastiaan Zeeff <sebastiaan.zeeff@gmail.com>

--- async_rediscache/redis_scripts/redisqueue_reschedule_all_client_tasks.lua
+++ async_rediscache/redis_scripts/redisqueue_reschedule_all_client_tasks.lua
@@ -1,6 +1,14 @@
 local queue_size = redis.call("LLEN", KEYS[2])
 local client_tasks = redis.call("LRANGE", KEYS[2], 0, queue_size)
 
+-- For Lua version compatibility, try picking the right version of unpack
+local unpack_function
+if unpack == nil then
+  unpack_function = table.unpack
+else
+  unpack_function = unpack
+end
+
 if queue_size > 0 then
   redis.call("LPUSH", KEYS[1], unpack(client_tasks))
 end
